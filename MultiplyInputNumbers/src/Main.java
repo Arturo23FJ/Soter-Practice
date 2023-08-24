ba9a7a745bcc31cc23159743b5e3ef31e3707189
unknown <turkovics.viktor@gmail.com>
2023-08-24 16:03:59 +0200

    divideWithScanner method added

--- MultiplyInputNumbers/src/Main.java
+++ MultiplyInputNumbers/src/Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(multiplyWithScanner(100));
+//        System.out.println(multiplyWithScanner(100));
+        System.out.println(divideWithScanner(100));
     }
 
     public static int multiplyWithScanner(int limit){
@@ -19,5 +20,22 @@ public class Main {
         }
 
         return result;
+    }
+
+    public static double divideWithScanner(double dividend){
+        Scanner scanner = new Scanner(System.in);
+        double divisor;
+        double remainder = 0.0;
+
+        while (scanner.hasNextDouble()){
+            divisor = scanner.nextDouble();
+            remainder = dividend / divisor;
+            dividend = remainder;
+            if(remainder <= 2.0){
+                break;
+            }
+        }
+
+        return remainder;
     }
 }
