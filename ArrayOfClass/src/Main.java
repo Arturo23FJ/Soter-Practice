77e7d2d75938d7930e2aba9e4ae369216f34a9a6
unknown <turkovics.viktor@gmail.com>
2023-08-29 21:23:44 +0200

    priceOfAllCars method initialized in Main class

--- ArrayOfClass/src/Main.java
+++ ArrayOfClass/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car[] cars = new Car[]{new Car("Audi", new int[]{150, 800, 1200, 970}),
+                                new Car("Alfa Romeo", new int[]{170, 890, 1400, 600}),
+                                new Car("Mitsubishi", new int[]{190, 910, 1540, 1150})};
+
+        System.out.println("The price of the cars is: " + priceOfAllCars(cars));
+    }
+
+    public static int priceOfAllCars(Car[] cars){
+        int sum = 0;
+
+        for (Car car : cars) {
+            sum += car.priceOfCar();
+        }
+        return sum;
     }
 }
