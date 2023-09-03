906bf2f31688c4541260706a34280960e3afb41e
unknown <turkovics.viktor@gmail.com>
2023-09-03 23:09:12 +0200

    Rock-Paper-Scissors game created, Player class created with choices, randomChoice and setChoiceByInput methods, whoWins method created in Main class

---
+++ RockPaperScissorsGame/src/game/Main.java
@@ -0,0 +1,35 @@
+package game;
+/*Exercise:
+Rock-Paper-Scissors game: we play until someone gets 2 wins. The computer's choice should be random.
+0 = rock, 1 = paper, 2 = scissors
+There should be a player class, we should create two objects of it in the Main class
+We should have a setter to set our choice
+There should be  method which sets random choice
+In the Main class, one of the objects should be the computer, one of them should be us
+We should input our choice, the computer should call the random choice method
+Let's check who won each round and summarize it in the end
+ */
+public class Main {
+    public static void main(String[] args) {
+        Player computer = new Player();
+        Player player = new Player();
+
+        whoWins(computer, player);
+    }
+
+    public static void whoWins(Player computer, Player player){
+        String computerChoice = computer.randomChoice();
+        String playerChoice = player.setChoiceByInput();
+
+        if (computerChoice.equals(playerChoice)){
+            System.out.println("Draw! Player chose: " + playerChoice + ", Computer chose: " + computerChoice);
+        } else if ((computerChoice.equals("Rock") && playerChoice.equals("Scissors")) ||
+                (computerChoice.equals("Paper") && playerChoice.equals("Rock")) ||
+                (computerChoice.equals("Scissors") && playerChoice.equals("Paper"))){
+            System.out.println("Computer wins! Computer chose " + computerChoice +
+                    ", Player chose: " + playerChoice);
+        } else {
+            System.out.println("Player wins! Player chose: " + playerChoice + ", Computer chose: " + computerChoice);
+        }
+    }
+}
---
+++ RockPaperScissorsGame/src/game/Player.java
@@ -0,0 +1,43 @@
+package game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Player {
+    private String choice;
+
+    public Player() {}
+
+
+    private String choices(int choice){
+        if (choice == 0){
+            return this.choice = "Rock";
+        } else if(choice == 1){
+            return this.choice = "Paper";
+        } else
+            return this.choice = "Scissors";
+    }
+
+    public String randomChoice() {
+        Random random = new Random();
+        int numberOfChoice = random.nextInt(3);
+
+        return choices(numberOfChoice);
+    }
+
+    public String setChoiceByInput(){
+        System.out.println("Please enter your choice! 0 = Rock, 1 = Paper, 2 = Scissors");
+        Scanner scanner = new Scanner(System.in);
+        int inputChoice = scanner.nextInt();
+        String choice = "";
+
+        if(inputChoice < 0 || inputChoice > 2) {
+            System.out.println("Please write a valid choice! 0 = Rock, 1 = Paper, 2 = Scissors");
+            scanner.hasNextInt();
+        }
+        choice = choices(inputChoice);
+
+        return choice;
+    }
+
+}
