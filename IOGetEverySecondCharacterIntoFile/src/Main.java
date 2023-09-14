c7c0d1da12940cf8129deef3a5327d1a11256276
unknown <turkovics.viktor@gmail.com>
2023-09-14 14:50:20 +0200

    IOGetEverySecondCharacterIntoFile project created, text file is being read and every second character is being put into another file

---
+++ IOGetEverySecondCharacterIntoFile/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try(FileReader fr = new FileReader("text.txt");
+            FileWriter fw = new FileWriter("text2.txt")){
+            int character;
+            int characterCount = 0;             //Variable to get each character from the text file
+
+            while ((character = fr.read()) != -1){
+                if((characterCount % 2) == 0) {          //Getting every second character with the remainder operator
+                    fw.write((char)character);          //Converting the type int character to char and writing it to another text file
+                }
+                ++characterCount;   //Incrementing the character count to get the next one
+            }
+        }catch (FileNotFoundException fne){
+            System.out.println(fne.getMessage());
+        }catch (IOException ioe){
+            System.out.println(ioe.getMessage());
+        }
+    }
+}
